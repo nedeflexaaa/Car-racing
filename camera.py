321bca4ff8d72d1599a01169342c1a21e35652d8
frotanis <bubelkim12@gmail.com>
2026-05-07 18:37:08 +0300

    add basic camera

---
+++ camera.py
@@ -0,0 +1,15 @@
+import pygame
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+class Camera:
+    def __init__(self, map_width, map_height):
+        # map_width та map_height - розміри всієї картинки траси
+        self.camera = pygame.Rect(0, 0, map_width, map_height)
+        self.width = map_width
+        self.height = map_height
+
+    def update(self, target):
+        # 1. Рахуємо ідеальний центр, ніби обмежень немає
+        x = -target.rect.centerx + int(SCREEN_WIDTH / 2)
+        y = -target.rect.centery + int(SCREEN_HEIGHT / 2)
