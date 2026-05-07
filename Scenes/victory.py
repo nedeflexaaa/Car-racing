56f9a1afa75a11e022502d298fa183492e37fe3d
Anna-nya <annarusavska1@gmail.com>
2026-05-07 20:27:15 +0300

    Assets for victory screen

--- Scenes/victory.py
+++ Scenes/victory.py
@@ -3,3 +3,14 @@ from settings import *
 
 
 class VictoryScene:
+    def __init__(self, screen):
+        self.screen = screen
+        self.font = pygame.font.SysFont(None, 50)
+
+        self.images = []
+        image_paths = [
+            "Assets/victory/like.jpg",
+            "Assets/victory/yalta.jpg",
+            "Assets/victory/homer.jpg",
+            "Assets/victory/breaking_bad.jpg"
+        ]
